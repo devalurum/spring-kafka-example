d482ab97df60f3c7c49b405fd20b30b718744d7d
Alexander Urumov <sahaheh@mail.ru>
2022-05-05 16:39:39 +0800

    fixed error with InvalidDefinitionException for Jackson

--- src/main/java/org/devalurum/kafka/example/model/User.java
+++ src/main/java/org/devalurum/kafka/example/model/User.java
@@ -1,10 +1,14 @@
 package org.devalurum.kafka.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private long userId;
     private String firstName;
